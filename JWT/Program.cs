5bf7f3f27b20e6d131478bd45bcb4f2cacb0c2f9
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-17 17:52:32 +0330

    Edited on Nov 17, 2025

--- JWT/Program.cs
+++ JWT/Program.cs
@@ -19,7 +19,9 @@ namespace JWTwithSwagger
             // WebApplicationBuilder object
             var builder = WebApplication.CreateBuilder(args);
 
+
             // --------------------------------------------------------
+
 
             // Load JWT settings            
             var jwtSettings = builder.Configuration.GetSection("JwtSettings");
@@ -35,7 +37,7 @@ namespace JWTwithSwagger
 
             // (1)
             // Configure authentication using a JWT Token
-            // Authorization is performed by extracting and validating a JWT token from the authorization request header.
+            // Authentication is performed by extracting and validating a JWT token from the authorization request header.
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -47,19 +49,24 @@ namespace JWTwithSwagger
                         ValidateIssuerSigningKey = true,
                         ValidIssuer = jwtSettings["Issuer"],
                         ValidAudience = jwtSettings["Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                        IssuerSigningKey = new SymmetricSecurityKey(key) // Symmetric Security Key is used to sign and validate the token.
+                        // A Symmetric Security Key means the same key is used for both signing and validation.
                     };
                 });
 
 
             // (2)
-            // Adding th symmetric security key as a service so that it could be injected into other methods elsewhere.
+            // Adding the SymmetricSecurityKey as a singleton service.
+            // This means that the same instance of SymmetricSecurityKey will be used throughout the application's lifetime.
             builder.Services.AddSingleton(new SymmetricSecurityKey(key));
 
             // (3)
+            // Adding authorization services to the application.
             builder.Services.AddAuthorization();
 
             // (4)
+            // Adding controller services to the application.
+            // In other words, enabling the use of MVC controllers.
             builder.Services.AddControllers();
 
             // (5)
@@ -68,8 +75,9 @@ namespace JWTwithSwagger
             {
                 // (a)
                 options.SwaggerDoc("v1.0", new OpenApiInfo { Title = "My Test API", Version = "v1.0" });
-                
+
                 // (b)
+                // 'AddSecurityDefinition' is used to define a security scheme that can be used by the API.
                 options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Name = "Authorization", // The request header name for JWT
@@ -81,8 +89,10 @@ namespace JWTwithSwagger
                 });
 
                 // (c)
+                // 'AddSecurityRequirement' is used to specify that the API requires a certain security scheme to be used.
                 // Now we’re saying: "This API requires JWT security."
-                // This class behaves like a dictionary.                
+                // This class behaves like a dictionary.
+                // OpenApiSecurityRequirement is essentially a Dictionary<OpenApiSecurityScheme, IList<string>>.
                 options.AddSecurityRequirement(new OpenApiSecurityRequirement // So inside the curly braces, we are adding key-value pairs.
                 {
                     {
@@ -91,8 +101,8 @@ namespace JWTwithSwagger
                         {
                             Reference = new OpenApiReference // We’re using a reference to the "Bearer" scheme defined above.
                             {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
+                                Type = ReferenceType.SecurityScheme, // Indicating that this reference is to a security scheme.
+                                Id = "Bearer"                        // The ID of the security scheme we are referencing.
                             }
                         },
                         // value
@@ -100,6 +110,7 @@ namespace JWTwithSwagger
                     }
                 });
             });
+
 
             // --------------------------------------------------------
 
@@ -117,20 +128,26 @@ namespace JWTwithSwagger
             // (1)
             app.UseAuthentication();
             app.UseAuthorization();
-            
+
             // (2)
+            // Enabling attribute routing for controllers.
             app.MapControllers();
-            
+
             // (3)
             // In older versions of .NET, app.UseRouting() or app.UseEndpoints() can be used instead.
             // app.MapGet() is the minimal APIs model in .NET 6+.
+            // Here, we define a simple GET endpoint at the root URL ("/") that returns a plain text message.
             app.MapGet("/", () => "A simple JWT Token application!");
 
             // (4)
+            // Enforcing HTTPS redirection and HSTS (HTTP Strict Transport Security)
+            // HSTS is a web security policy mechanism that helps to protect websites
+            // from man-in-the-middle attacks by forcing clients to use HTTPS.
+            // In fact, HSTS prevents downgrade attacks and cookie hijacking.
             app.UseHsts();
 
             // (5)
-            // Enabling Swagger on: http(s)://localhost:port_number/swagger
+            // Enabling Swagger in the Dev Environment on: http(s)://localhost:port_number/swagger
             if (app.Environment.IsDevelopment())
             {
                 // The Swagger middleware handles all incoming requests.
@@ -142,6 +159,8 @@ namespace JWTwithSwagger
                     // Add custom style
                     c.InjectStylesheet("/css/swagger.css");
 
+                    // 'swagger.json' file is generated by the Swagger middleware.
+                    // It contains the OpenAPI specification for the API.
                     c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "My API V1"); // (client-side mapping)
 
                     // Set a custom route, e.g., localhost:port_number/swagger
@@ -150,10 +169,12 @@ namespace JWTwithSwagger
             }
 
             // (6)
+            // Serving static files (e.g., CSS files for Swagger UI) in the wwwroot folder
             app.UseStaticFiles();
 
             // --------------------------------------------------------
 
+            // Running the application
             app.Run();
         }
     }
